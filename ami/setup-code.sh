e63fe4e06579bdfed0de17e14627d49061ca6664
Sumit Sontakke <smsontakke@ymail.com>
2022-08-04 07:01:44 +0100

    configuring dy github repo

--- ami/setup-code.sh
+++ ami/setup-code.sh
@@ -1,14 +1,11 @@
 sleep 30
 sudo yum update -y
 sleep 10
-echo "installing git"
-sudo yum install git -y
-sudo yum install python3-pip -y
-# sudo yum install libpq-dev python-dev -y
-# update pip3 to latest version
-python3 -m pip install --upgrade pip
-# upgrade pillow library
-# python3 -m pip install --upgrade Pillow
+
+
+mkdir -p /var/log/django_app
+sudo chown -Rf ec2-user /var/log/django_app
+sudo chmod -Rf 775 /var/log/django_app
 
 # update PATH 
 export PATH=$PATH:/usr/local/bin
@@ -17,25 +14,33 @@ sleep 1
 source ~/.bash_profile
 
 sleep 10
-mkdir -p ~/.ssh
-touch /root/.ssh/id_rsa
-cat /tmp/superrirya_github >> ~/.ssh/id_rsa
+echo "installing git"
+sudo yum install git -y
+sudo yum install python3-pip -y
+# sudo yum install libpq-dev python-dev -y
+# update pip3 to latest version
+python3 -m pip install --upgrade pip
 
-chmod +x /tmp/mkssh.sh
-/tmp/mkssh.sh
+# sleep 10
+# mkdir -p ~/.ssh
+# touch /root/.ssh/id_rsa
+# cat /tmp/superrirya_github >> ~/.ssh/id_rsa
 
-git config --global user.name "superriya"
-git config --global user.email "supriya-sontakke@hotmail.com"
-eval $(ssh-agent -s)
-chmod 600 ~/.ssh/id_rsa 
-ssh-add ~/.ssh/id_rsa
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+# chmod +x /tmp/mkssh.sh
+# /tmp/mkssh.sh
+
+# git config --global user.name "superriya"
+# git config --global user.email "supriya-sontakke@hotmail.com"
+# eval $(ssh-agent -s)
+# chmod 600 ~/.ssh/id_rsa 
+# ssh-add ~/.ssh/id_rsa
+# ssh-keyscan github.com >> ~/.ssh/known_hosts
 #rm -f /tmp/superrirya_github
 
 pip3 install psycopg2-binary
 
 sleep 10
-git clone --branch main https://github.com/own-space/django-github-cicd.git /src
+git clone --branch ${github_branch} ${GITHUB_REPO} /src
 me=`whoami`
 sudo chown -Rf $me /src
 
@@ -80,6 +85,5 @@ python3 /src/application/manage.py makemigrations
 python3 /src/application/manage.py migrate
 
 sudo chown -Rf ec2-user /src
-
 # update settings file to allow hosts from other domains
 sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\"*\"\]/g' /src/application/django_app/settings.py
